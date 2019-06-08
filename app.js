aa28fad73fb2c8d62e05e2f5dddb6da6f90bcc00
AkashSReddy <akashsreddy98@gmail.com>
2019-06-08 23:45:55 +0530

    Database model added

--- app.js
+++ app.js
@@ -24,9 +24,13 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 
-mongoose.connect("mongodb://localhost/monetization_api", {
-  useNewUrlParser: "true"
-});
+mongoose.connect(
+  "mongodb://localhost/monetization_api",
+  { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false },
+  err => {
+    if (!err) console.log("Connection successful");
+  }
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var request = require("request-promise");
+var Data = require("../models/data");
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.json({ message: "working" });
@@ -23,4 +24,36 @@ router.get("/getData", async (req, res, next) => {
     res.json({ message: "error" });
   }
 });
+
+router.post("/newData", async (req, res, next) => {
+  try {
+    let data = new Data();
+    data.name = req.body.name;
+    data.type = req.body.type;
+    data.group = req.body.group;
+    data.records = req.body.records;
+    await data.save();
+    var options = {
+      method: "POST",
+      uri: "http://localhost:3000/api/Data",
+      body: {
+        $class: "org.me.cto.Data",
+        dataId: data.id,
+        forTrade: true,
+        owner: "resource:org.me.cto.User#1"
+      },
+      json: true
+    };
+    let response = await request(options);
+    console.log(response);
+    res.json(response);
+    // var stuff = JSON.parse(data.records);
+    // res.json(stuff);
+  } catch (error) {
+    console.log(error);
+    res.json({ message: "error" });
+  }
+});
+
+router.get("/factors", (req, res, next) => {});
 module.exports = router;
